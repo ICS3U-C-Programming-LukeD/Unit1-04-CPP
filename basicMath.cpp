d5079c50ab3fa6cd53a8955900ddf5f9a63e7e81
Luke Di Bert <luke.di-bert@stu.ocsb.ca>
2025-02-12 18:11:40 +0000

    added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Luke Di Bert All rights reserved.
+//
+// Created by: Luke Di Bert
+// Date: Feb 12, 2025
+// Performs basic math operations
+
+#include <cmath>
+#include <iostream>
+ int main() {
+     std::cout << "5 + 5 = " << 5 + 5 << std::endl;
+     std::cout << "6 - 2 = " << 6 - 2 << std::endl;
+     std::cout << "3 x 6 = " << 3 * 6 << std::endl;
+     std::cout << "14 / 12 = " << 14 / 12 << std::endl;
+     std::cout << "4 ^ 3 = " << pow(4, 3) << std::endl;
+     std::cout << "√16 = " << sqrt(16) << std::endl;
+}
